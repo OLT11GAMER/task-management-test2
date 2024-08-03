882b634126a24468964f1df46a3ad4c7f828efd6
OLT11GAMER <93716411+OLT11GAMER@users.noreply.github.com>
2024-08-03 09:12:22 +0200

    bh

--- k8s/providers.tf
+++ k8s/providers.tf
@@ -26,9 +26,6 @@ provider "docker" {
 }
 
 provider "kubernetes" {
-  host                   = google_container_cluster.primary.endpoint
-  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
-  token                  = data.google_client_config.default.access_token
 }
 
 # provider "kubernetes"{
